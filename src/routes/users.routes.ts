9f3ff7180211af643c42c5934025943541b340a9
Camila Sales <salescamila1@gmail.com>
2020-07-21 15:01:46 -0400

    Atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-
-import CreateUserService from '../services/CreateUserService';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -23,8 +25,8 @@ usersRouter.post('/', async (request, response) => {
     delete user.password;
     
     return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
   }
 });
 
@@ -33,7 +35,20 @@ usersRouter.patch(
   ensureAuthenticated, 
   upload.single('avatar'), 
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   },
 );
 
