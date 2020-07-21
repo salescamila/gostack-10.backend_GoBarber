86d7a71a06da572c2103428d210f9cc2ed24831d
Camila Sales <salescamila1@gmail.com>
2020-07-21 16:09:11 -0400

    Lidando com erros

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -13,22 +13,18 @@ import AuthenticateUserSerivce from '../services/AuthenticateUserService';
  const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserSerivce();
+  const authenticateUser = new AuthenticateUserSerivce();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
