ac8a8dc75db12b82481971cb4e0fd7e3eada2d43
Camila Sales <salescamila1@gmail.com>
2020-08-20 17:27:13 -0400

    Cors adicionado

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+app.use(cors());
 app.use(routes);
 
 app.use(
